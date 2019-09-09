3b4f34ef861b5d881d6482b65a0e9a570d6118f0
Rostyslav Yatsura <floxaz1@gmail.com>
2019-09-09 23:53:42 +0200

    created footer and implemented router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SolutionsComponent } from './solutions/solutions.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'solutions', component: SolutionsComponent},
+  {path: 'lifestyle', component: LifestyleComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
+  declarations: [
+    HomeComponent,
+    SolutionsComponent,
+    LifestyleComponent,
+    PageNotFoundComponent
+  ],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
---
+++ src/app/lifestyle/lifestyle.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-lifestyle',
+    templateUrl: './lifestyle.component.html',
+    styleUrls: ['./lifestyle.component.scss']
+})
+export class LifestyleComponent {}
